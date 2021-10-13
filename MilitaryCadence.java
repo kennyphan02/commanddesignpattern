03d173989aa86a0ec8a055104fa2f51048e46fd3
Kenny Phan <kenobusiness1@gmail.com>
2021-10-13 17:23:13 -0400

    Finished methods for MilitaryCadence.java file

--- MilitaryCadence.java
+++ MilitaryCadence.java
@@ -1,15 +1,69 @@
+import java.util.concurrent.TimeUnit;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class MilitaryCadence {
-    public MilitaryCadence(){
-
-    }
+    public MilitaryCadence(){}
     public void singIDontKnow(){
-
+        final String FILE_NAME = "/home/kenny/commanddesignpattern/idontknow.txt";
+        try {
+            File file = new File(FILE_NAME);
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+                TimeUnit.MILLISECONDS.sleep(1000);
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+            scanner.close();
+          } catch (FileNotFoundException e) {
+            System.out.println("Sorry, we could not properly read the cadence file");
+            e.printStackTrace();
+          } catch(Exception e){
+            e.printStackTrace();
+          }
     }
     public void singEverywhereWeGo(){
+        final String FILE_NAME = "/home/kenny/commanddesignpattern/everywhere.txt";
+        try {
+            File file = new File(FILE_NAME);
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+                TimeUnit.MILLISECONDS.sleep(1000);
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+            scanner.close();
+          } catch (FileNotFoundException e) {
+            System.out.println("Sorry, we could not properly read the cadence file");
+            e.printStackTrace();
+          } catch(Exception e){
+            e.printStackTrace();
+          }
 
     }
     public void singInArmy(){
-        
+        final String FILE_NAME = "/home/kenny/commanddesignpattern/inarmy.txt";
+        try {
+            File file = new File(FILE_NAME);
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+                TimeUnit.MILLISECONDS.sleep(1000);
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+            scanner.close();
+          } catch (FileNotFoundException e) {
+            System.out.println("Sorry, we could not properly read the cadence file");
+            e.printStackTrace();
+          } catch(Exception e){
+            e.printStackTrace();
+          }
+
     }
     
 }
