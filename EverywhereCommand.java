2f5bb45422f07fd6cc1a93e4c9660f5fc3a13b68
danaiangelidis <angelidisdanai@gmail.com>
2021-10-13 22:19:40 -0400

    final push

--- EverywhereCommand.java
+++ EverywhereCommand.java
@@ -1,11 +1,18 @@
 public class EverywhereCommand implements Command {
-    
+
     private MilitaryCadence cadence;
     
+    /**
+     * Constructor for the Everywhere cadence.
+     * @param cadence The cadence that was chosen to be played.
+     */
     public EverywhereCommand(MilitaryCadence cadence) {
         this.cadence = cadence;
     }
 
+    /**
+     * Executes the singEverywhere method.
+     */
     public void execute() {
         this.cadence.singEverywhereWeGo();
     }
