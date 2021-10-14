0d85808ad6e74be772e4720f4e55e3b4a327d2b9
Danielthewin <Danielthewin12@gmail.com>
2021-10-13 21:25:26 -0400

    edited everywherecommand

--- InArmyCommand.java
+++ InArmyCommand.java
@@ -3,14 +3,8 @@ public class InArmyCommand implements Command {
     public InArmyCommand(MilitaryCadence cadence) {
 
     }
-<<<<<<< HEAD
     public void execute(){
         cadence.singInArmy();
-=======
-    
-    public void execute() {
-        
->>>>>>> 5c8433210bb71b40d4298f9ee0ca406f03156fdc
     }
     
 }
