ecad170437a52e7f804ff897c98ee34a2ac71379
Danielthewin <Danielthewin12@gmail.com>
2021-10-13 22:17:11 -0400

    javadoc

--- InArmyCommand.java
+++ InArmyCommand.java
@@ -1,9 +1,16 @@
 public class InArmyCommand implements Command {
     private MilitaryCadence cadence;
 
+    /**
+     * Constructor for the In Army Command cadence.
+     * @param cadence The cadence that was chosen to be played.
+     */
     public InArmyCommand(MilitaryCadence cadence) {
         this.cadence = cadence;
     }
+    /**
+    * exectues the singInArmy method.
+    */
     public void execute(){
         this.cadence.singInArmy();
     }
